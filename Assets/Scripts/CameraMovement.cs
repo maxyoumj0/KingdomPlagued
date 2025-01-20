9a0051883e2cd3a9ab7e1bd09da463afe58b3311
Henry Jin <jinhe@umich.edu>
2025-01-19 16:00:22 -0800

    Implement edge camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,33 +6,50 @@ using UnityEngine.UIElements;
 
 public class PlayerCamera : NetworkBehaviour
 {
-    public float MoveSpeed = 5.0f;
-    public int ZoomLevel = 5;
+    [SerializeField] private float _moveSpeed = 5.0f;
+    [SerializeField] private int _zoomLevel = 5;
+    [SerializeField] private float _edgeSize = 30f;
+
     private InputAction _moveAction;
     private InputAction _zoomAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         _moveAction = InputSystem.actions.FindAction("Move");
         _zoomAction = InputSystem.actions.FindAction("Zoom");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Vector2 moveValue = _moveAction.ReadValue<Vector2>();
-        transform.position += new Vector3(moveValue.x, 0, moveValue.y) * MoveSpeed * Time.deltaTime;
+        transform.position += new Vector3(moveValue.x, 0, moveValue.y) * _moveSpeed * Time.deltaTime;
+
+        if (Application.isFocused) {
+            if (Input.mousePosition.x > Screen.width - _edgeSize) {
+                transform.position += Vector3.right * _moveSpeed * Time.deltaTime;
+            }
+            if (Input.mousePosition.x < _edgeSize) {
+                transform.position -= Vector3.right * _moveSpeed * Time.deltaTime;
+            }
+            if (Input.mousePosition.y > Screen.height - _edgeSize) {
+                transform.position += Vector3.forward * _moveSpeed * Time.deltaTime;
+            }
+            if (Input.mousePosition.y < _edgeSize) {
+                transform.position -= Vector3.forward * _moveSpeed * Time.deltaTime;
+            }
+        }
 
         float zoomValue = _zoomAction.ReadValue<float>();
         if (zoomValue > 0)
         {
-            ZoomLevel += 1;
+            _zoomLevel += 1;
             transform.position -= new Vector3(0, 50f, 0) * Time.deltaTime;
         }
         else if (zoomValue < 0)
         {
-            ZoomLevel -= 1;
+            _zoomLevel -= 1;
             transform.position -= new Vector3(0, -50f, 0) * Time.deltaTime;
         }
     }
